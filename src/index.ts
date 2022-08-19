773ca59a9a2e535c1549db1eb7facc11518032c8
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2022-08-18 22:20:10 -0400

    Created Courses and StudyGroup types

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import courses from "./courses";
+import studyGroups from "./studyGroup";
+
+type Course = {
+  id: number;
+  studyGroupId: number;
+  title: string;
+  keywords: string[];
+  eventType: string;
+};
+
+type StudyGroup = {
+  id: number;
+  courseId: number;
+  title: string;
+  keywords: string[];
+  eventType: string;
+};
+
+function searchEvent(courses: Course[], groups: StudyGroup[]) {}
